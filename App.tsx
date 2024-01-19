41a874994ad7a591f4c5baa3d3413f3a1f56e783
KUROSAKI <xxxbijumrzaxxx@gmail.com>
2024-01-19 21:07:21 +0700

    main_project

--- App.tsx
+++ App.tsx
@@ -1,118 +1,177 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, Text, StatusBar, View, Image, TouchableOpacity } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+const App = () => {
+    const [activeTab, setActiveTab] = useState('description');
+    const [requirementsTab, setRequirementsTab] = useState('minimum');
+    return (
+      <>
+        <StatusBar barStyle="default" />
+          <SafeAreaView style={styles.mainContainer}>
+              <ScrollView>
+                  <Image source={require('./assets/image/poster.jpg')} style={styles.poster} />
+                  <View style={styles.informationContainer}>
+                      <View style={styles.ois}>
+                      <TouchableOpacity
+                          style={[styles.buttomTab, activeTab === 'description' && styles.activeTab]}
+                          onPress={() => setActiveTab('description')}
+                      >
+                          <Text style={styles.textTab}>Описание</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                          style={[styles.buttomTab, activeTab === 'info' && styles.activeTab]}
+                          onPress={() => setActiveTab('info')}
+                      >
+                          <Text style={styles.textTab}>Инфо</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                          style={[styles.buttomTab,  activeTab === 'requirements' && styles.activeTab]}
+                          onPress={() => setActiveTab('requirements')}
+                      >
+                          <Text style={styles.textTab}>Системные требования</Text>
+                      </TouchableOpacity>
+                      </View>
+                      {activeTab === 'description' && (
+                          <Text style={styles.textInfoContainer}>
+                              Выжить — это лишь первый шаг. Resident Evil 4, настоящий гигант индустрии,
+                              возрождается. Вас ждут обновленный игровой процесс, переработанный сюжет и крайне
+                              детализированная графика. Вновь переживите кошмар, полностью изменивший жанр ужасов.
+                          </Text>
+                      )}
+                      {activeTab === 'info' && (
+                          <View>
+                              <Text style={styles.textInfoContainer}>Оценка в Steam: Крайне положительные</Text>
+                              <Text style={styles.textInfoContainer}>Дата выхода: 24/03/2020/</Text>
+                              <Text style={styles.textInfoContainer}>Разработчик: CAPCOM Co., Ltd.</Text>
+                              <Text style={styles.textInfoContainer}>Издатель: CAPCOM Co., Ltd.</Text>
+                          </View>
+                      )}
+                      {activeTab === 'requirements' && (
+                          <>
+                              <View style={styles.requirementsTabContainer}>
+                                  <TouchableOpacity
+                                      style={[styles.minimunTab, requirementsTab === 'minimum' && styles.activeTab]}
+                                      onPress={() => setRequirementsTab('minimum')}
+                                  >
+                                      <Text style={styles.textTab}>Минимальные</Text>
+                                  </TouchableOpacity>
+                                  <TouchableOpacity
+                                      style={[styles.minimunTab, requirementsTab === 'recommended' && styles.activeTab]}
+                                      onPress={() => setRequirementsTab('recommended')}>
+                                      <Text style={styles.textTab}>Рекомендуемые</Text>
+                                  </TouchableOpacity>
+                              </View>
+                              {requirementsTab === 'minimum' && (
+                                  <View>
+                                      <Text style={styles.textRequiremntContainer}>ОС: Windows 10 (64 bit)</Text>
+                                      <Text style={styles.textRequiremntContainer}>Процессор: AMD Ryzen 3 1200 / Intel Core
+                                          i5-7500</Text>
+                                      <Text style={styles.textRequiremntContainer}>Видеокарта: AMD Radeon RX 560 with 4GB VRAM /
+                                          NVIDIA GeForce GTX 1050 Ti with 4GB VRAM
+                                      </Text>
+                                      <Text style={styles.textRequiremntContainer}>RAM: 8 GB</Text>
+                                      <Text style={styles.textRequiremntContainer}>DISK: 70GB </Text>
+                                  </View>
+                              )}
+                              {requirementsTab === 'recommended' && (
+                                  <View>
+                                      <Text style={styles.textRequiremntContainer}>ОС: Windows 11 (64 bit)</Text>
+                                      <Text style={styles.textRequiremntContainer}>Процессор: AMD Ryzen 3 1200 / Intel Core
+                                          i5-7500</Text>
+                                      <Text style={styles.textRequiremntContainer}>Видеокарта: AMD Radeon RX 560 with 4GB VRAM /
+                                          NVIDIA GeForce GTX 1050 Ti with 4GB VRAM
+                                      </Text>
+                                      <Text style={styles.textRequiremntContainer}>RAM: 8 GB</Text>
+                                      <Text style={styles.textRequiremntContainer}>DISK: 70GB </Text>
+                                  </View>
+                              )}
+                          </>
+                      )}
+                  </View>
+                  <View style={styles.marketplacesContainer}>
+                      <Text style={styles.textTitle}>Торговые площадки</Text>
+                  </View>
+              </ScrollView>
+          </SafeAreaView>
+      </>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+const styles = StyleSheet.create( {
+    mainContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#121a24',
+    },
+    marketplacesContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        position: 'relative',
+        backgroundColor: '#ffffff',
+        borderRadius: 35,
+        minHeight: 150,
+        marginTop: 25,
+    },
+    informationContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        position: "relative",
+        backgroundColor: '#ffffff',
+        borderRadius: 35,
+        minHeight: 150,
+        marginTop: -30,
+        padding: 30,
+    },
+    ois: {
+        flexDirection: 'row',
+    },
+    poster: {
+        alignItems: 'flex-start',
+        height: 230,
+        width: '100%',
+        resizeMode: 'cover',
+    },
+    textTab: {
+        fontSize: 16,
+        color: '#000000',
+        textAlign: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    textInfoContainer: {
+        fontSize: 16,
+        color: '#000000',
+        marginTop: 5,
+    },
+    textRequiremntContainer: {
+        fontSize: 16,
+        color: '#000000',
+        marginTop: 5,
+    },
+    activeTab: {
+        borderBottomColor: 'blue',
+    },
+    buttomTab: {
+        borderBottomWidth: 2,
+        borderBottomColor: 'transparent',
+    },
+    minimunTab: {
+        borderBottomWidth: 2,
+        borderBottomColor: 'transparent',
+    },
+    requirementsTabContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 2,
+        padding: 5,
+    },
+    textTitle: {
+        fontSize: 16,
+        color: '#000000',
+        textAlign: 'center',
+        marginTop: 10,
+    },
 });
 
 export default App;
